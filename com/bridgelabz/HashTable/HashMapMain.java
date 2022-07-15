3b2faeb48bb8e49cdf50662dffeb506e7ff7d838
RahulPatil-N7 <rahul.patil.3125@gmail.com>
2022-07-15 22:43:45 +0530

    Added : welcome message.

---
+++ com/bridgelabz/HashTable/HashMapMain.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.HashTable;
+
+public class HashMapMain {
+
+	public static void main(String[] args) {
+		
+		//printing welcome message
+		System.out.println("Welcome to Hash Table Program.\n");
+		
+	}
+
+}
