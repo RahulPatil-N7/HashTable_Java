d5ca31e2bf1e01fff5deeadc1e783bd2d8c18366
RahulPatil-N7 <rahul.patil.3125@gmail.com>
2022-07-15 23:07:30 +0530

    Added : Find frequency of word in given sentence.

---
+++ com/bridgelabz/HashTable/INode.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.HashTable;
+
+public interface INode<K> {
+	
+	//declaring node methods 
+	void setKey(K key);
+	K getKey();
+	void setNext(INode<K> next);
+	INode<K> getNext();
+
+}
